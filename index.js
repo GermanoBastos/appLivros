44bba20ffc6bcb6ca56e518698328b3a70feeaed
Germano Bastos <germano.silveira.neto@gmail.com>
2023-06-04 10:23:34 -0300

    kj

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-import express from 'express';
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req,res) =>{
-res.send('Exprexx funcionou')
+function getNextId(livros) {
+  if (livros.length === 0) {
+    return 1;
+  }
+  const lastLivro = livros[livros.length - 1];
+  return lastLivro.id + 1;
+}
 
-}) 
+app.get('/', (req, res) => {
+  try {
+    const dados = fs.readFileSync('./livros.json');
+    const livros = JSON.parse(dados);
+    res.json(livros);
+  } catch (error) {
+    console.error('Erro ao listar os livros:', error);
+    res.status(500).send('Erro ao listar os livros');
+  }
+});
+
+app.get('/livros', (req, res) => {
+  try {
+    const dados = fs.readFileSync('./livros.json');
+    const livros = JSON.parse(dados);
+    res.json(livros);
+  } catch (error) {
+    console.error('Erro ao listar os livros:', error);
+    res.status(500).send('Erro ao listar os livros');
+  }
+});
+
+app.post('/livros', (req, res) => {
+  try {
+    const dadosAtuais = fs.readFileSync('./livros.json');
+    const livrosAtuais = JSON.parse(dadosAtuais);
+    const novoLivro = req.body;
+    novoLivro.id = getNextId(livrosAtuais);
+    livrosAtuais.push(novoLivro);
+    fs.writeFileSync('./livros.json', JSON.stringify(livrosAtuais));
+    res.send('Livro adicionado com sucesso!');
+  } catch (error) {
+    console.error('Erro ao adicionar o livro:', error);
+    res.status(500).send('Erro ao adicionar o livro');
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`);
-  });
+  console.log(`Escutando a porta ${port}`);
+});
